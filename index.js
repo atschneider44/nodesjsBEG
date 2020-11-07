bf1422058ea69f6083f32c511e03dc8e28fd7456
Anna Schneider <at.schneider44@gmail.com>
2020-11-07 14:09:13 +0100

    update 2

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const http = require('http');
+const express = require('express');
 
 //create http server
 
 let server = http.createServer((req, res) => {
-    res.write('Hello World');
-    res.end();
+    if (req.url === '/'){
+        res.write('Hello World'); 
+        res.end();  
+    }
+    if (req.url === '/all'){
+        res.write('This is a json with a list of all users'); 
+        res.end();  
+    }
+    if (req.url === '/other'){
+        res.write('Something else'); 
+        res.end();  
+    } 
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
